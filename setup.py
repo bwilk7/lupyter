7cba36bd3f2433081b2e7374a67eb12fb1d0ffbf
Bryan Wilkinson <bwilk7@gmail.com>
2018-02-23 16:23:07 -0500

    Making setup.py work with my lua

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from distutils.core import setup, Extension
 lup_ext_mod = Extension("lup",
                           sources=["src/lup.c", "src/_lup.c"],
 #                          include_dirs=['/usr/local/include'],
-                          libraries=['lua'],
+                          libraries=['lua5.3'],
+                          extra_compile_args=['-llua5.3','-I/usr/include/lua5.3'],
+                          extra_link_args=['-llua5.3','-I/usr/include/lua5.3/']
                           #library_dirs=['/usr/local/lib']
                           )
 
